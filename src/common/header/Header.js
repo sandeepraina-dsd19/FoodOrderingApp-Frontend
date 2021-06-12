476b25df5bf630167fe565c8d9f585be8c5d76d0
Rajasekar <rajasekar241996@gmail.com>
2021-06-13 02:06:52 +0530

    Header page structure

---
+++ src/common/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    render() {
+        return (
+                <div>
+                 <p>Header Page  </p> 
+                </div>
+        )
+    }
+}
+
+export default Header;
