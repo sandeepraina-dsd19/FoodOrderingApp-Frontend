e395336543a2851a37858d3eab432ead44a695dd
Rajasekar <rajasekar241996@gmail.com>
2021-06-14 00:37:52 +0530

    Updated login part with Profile page

--- src/FoodOrdering.js
+++ src/FoodOrdering.js
@@ -6,7 +6,7 @@ import {Route, Switch} from "react-router-dom";
 
 // Imports of different pages in the application
 import Home from './screens/home/Home';
-
+import Profile from './screens/profile/Profile';
 
 // This class represents the whole FoodOrdering Application.
  
@@ -20,6 +20,7 @@ class FoodOrdering extends Component {
         return (
             <Switch>
                 <Route exact path='/' render={(props) => <Home {...props} baseUrl={this.baseUrl}/>}/>
+                <Route exact path='/profile' render={(props) => <Profile {...props} />}/>
             </Switch>
         )
     }
