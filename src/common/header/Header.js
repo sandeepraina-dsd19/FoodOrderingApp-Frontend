e716855309f4e1b734899c1feaa43fa253da6002
Rajasekar <rajasekar241996@gmail.com>
2021-06-13 16:18:46 +0530

    Completed logo module

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
 
+//importing material-ui styles
+import { withStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+//importing material-ui components
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import FastfoodIcon from '@material-ui/icons/Fastfood';
+import { Link } from 'react-router-dom';
+
+//styles for the header using breakpoints to make the header responsive
+const styles = theme => ({
+    appBar: {
+        backgroundColor: '#263238',
+        boxShadow: 'none',
+    },
+    headerTools: {
+        [theme.breakpoints.only('xs')]: {
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+        },
+    },
+    logo: {
+        '&:hover': {
+            backgroundColor: 'transparent !important',
+        },
+        cursor: 'default',
+    },
+
+});
+
 class Header extends Component {
 
     render() {
+        const { classes } = this.props;
         return (
-                <div>
-                 <p>Header Page  </p> 
-                </div>
+            <div>
+                <AppBar position="static" className={classes.appBar}>
+                    <Toolbar className={classes.headerTools}>
+                        <Link to={"/"} style={{
+                            color: 'white'
+                        }}>
+                            <IconButton disableRipple={true} className={classes.logo} edge="start" color="inherit"
+                                aria-label="app logo">
+                                <FastfoodIcon />
+                            </IconButton>
+                        </Link>
+                    </Toolbar>
+                </AppBar>
+            </div>
         )
     }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
