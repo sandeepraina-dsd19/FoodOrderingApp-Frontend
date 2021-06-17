f027378e701bfc50fdba8cf41e7e78f150019f9b
sandeepraina-dsd19 <sandeepraina.dsd19@iiitb.net>
2021-06-18 01:58:00 +0530

    Checkout page added and some issue resolved
    Checkout page added and some issue resolved

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import FoodOrdering from './FoodOrdering';
-import { BrowserRouter as Router } from "react-router-dom";
+import 'typeface-roboto';
+import {BrowserRouter as Router} from "react-router-dom";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Router>
